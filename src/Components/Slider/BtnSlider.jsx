baafddff09174b3c43b9d30fcd74426bf9478571
Anna Smolskaya <anna31rus@mail.ru>
2024-03-25 23:52:16 +0500

    slider created

---
+++ src/Components/Slider/BtnSlider.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import "./Slider.css";
+import leftArrow from "./icons/left-arrow.svg";
+import rightArrow from "./icons/right-arrow.svg";
+
+export default function BtnSlider({ direction, moveSlide }) {
+  console.log(direction, moveSlide);
+  return (
+    <button
+      onClick={moveSlide}
+      className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
+    >
+      <img src={direction === "next" ? rightArrow : leftArrow} />
+    </button>
+  );
+}
+
+BtnSlider.propTypes = {
+  direction: PropTypes.oneOf(['next', 'prev']).isRequired,
+  moveSlide: PropTypes.func.isRequired
+};
